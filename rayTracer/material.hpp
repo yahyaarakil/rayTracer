c9c4775c146689fe6b9d2045347d2e76dfb00dd0
Yahya Arakil <yahyaarakil@gmail.com>
2022-03-28 19:44:26 +0300

    Removed color, added Material, extended Scene.

---
+++ rayTracer/material.hpp
@@ -0,0 +1,14 @@
+//
+//  material.hpp
+//  rayTracer
+//
+//  Created by Yahya Arakil on 3/28/22.
+//
+
+#ifndef material_hpp
+#define material_hpp
+
+class Material{
+    
+};
+#endif /* material_hpp */
--- rayTracer/scene.hpp
+++ rayTracer/scene.hpp
@@ -8,14 +8,21 @@
 #ifndef scene_hpp
 #define scene_hpp
 
-#include "color.hpp"
+#include "vector3.hpp"
+#include "object.hpp"
+#include "camera.hpp"
+#include "material.hpp"
+#include <vector>
 
 class Scene{
 private:
-    Color backgroundColor;
+    Vector3 backgroundColor;
+    Vector3 ambientLight;
     int maxRecursionDepth;
     float shadowRayEpsilon;
     
+    std::vector<Material> materials;
+    std::vector<Object> objects;
 };
 
 #endif /* scene_hpp */
