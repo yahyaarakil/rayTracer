4363339364ee44d4323c100c4721ade802422663
Yahya Arakil <yahyaarakil@gmail.com>
2022-03-28 09:15:13 +0300

    Initial Commit

---
+++ rayTracer/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  rayTracer
+//
+//  Created by Yahya Arakil on 3/28/22.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
